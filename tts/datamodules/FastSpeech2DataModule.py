c31188b190737a38be8d1fc55b799ee41e9c5d30
hhhaaahhhaa <thomas1232121@gmail.com>
2022-09-29 11:45:18 +0800

    filename bug resolved

---
+++ tts/datamodules/FastSpeech2DataModule.py
@@ -0,0 +1,103 @@
+import torch
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, ConcatDataset
+
+import Define
+from tts.collates import FastSpeech2Collate
+from tts.datasets.FastSpeech2Dataset import FastSpeech2Dataset
+from .utils import EpisodicInfiniteWrapper
+
+
+class FastSpeech2DataModule(pl.LightningDataModule):
+    """
+    FastSpeech2Dataset + FastSpeech2Collate. 
+    """
+    def __init__(self, data_configs, model_config, train_config, algorithm_config, log_dir, result_dir):
+        super().__init__()
+        self.data_configs = data_configs
+        self.model_config = model_config
+        self.train_config = train_config
+        self.algorithm_config = algorithm_config
+
+        self.log_dir = log_dir
+        self.result_dir = result_dir
+        self.val_step = self.train_config["step"]["val_step"]
+
+        self.collate = FastSpeech2Collate()
+
+    def setup(self, stage=None):
+        if stage in (None, 'fit', 'validate'):
+            self.train_datasets = [
+                FastSpeech2Dataset(
+                    data_config['subsets']['train'],
+                    Define.DATAPARSERS[data_config["name"]],
+                    data_config
+                ) for data_config in self.data_configs if 'train' in data_config['subsets']
+            ]
+            self.val_datasets = [
+                FastSpeech2Dataset(
+                    data_config['subsets']['val'],
+                    Define.DATAPARSERS[data_config["name"]],
+                    data_config
+                ) for data_config in self.data_configs if 'val' in data_config['subsets']
+            ]
+            self.train_dataset = ConcatDataset(self.train_datasets)
+            self.val_dataset = ConcatDataset(self.val_datasets)
+            self._train_setup()
+            self._validation_setup()
+
+        if stage in (None, 'test', 'predict'):
+            self.test_datasets = [
+                FastSpeech2Dataset(
+                    data_config['subsets']['test'],
+                    Define.DATAPARSERS[data_config["name"]],
+                    data_config
+                ) for data_config in self.data_configs if 'test' in data_config['subsets']
+            ]
+            self.test_dataset = ConcatDataset(self.test_datasets)
+            self._test_setup()
+
+    def _train_setup(self):
+        if not isinstance(self.train_dataset, EpisodicInfiniteWrapper):
+            self.batch_size = self.train_config["optimizer"]["batch_size"]
+            self.train_dataset = EpisodicInfiniteWrapper(self.train_dataset, self.val_step*self.batch_size)
+
+    def _validation_setup(self):
+        pass
+
+    def _test_setup(self):
+        pass
+
+    def train_dataloader(self):
+        """Training dataloader"""
+        self.train_loader = DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            drop_last=True,
+            num_workers=Define.MAX_WORKERS,
+            collate_fn=self.collate.collate_fn(sort=False, mode="train")
+        )
+        return self.train_loader
+
+    def val_dataloader(self):
+        """Validation dataloader"""
+        self.val_loader = DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=0,
+            collate_fn=self.collate.collate_fn(sort=False, mode="train"),
+        )
+        return self.val_loader
+
+    def test_dataloader(self):
+        """Test dataloader"""
+        self.test_loader = DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            collate_fn=self.collate.collate_fn(sort=False, mode="test"),
+        )
+        return self.test_loader
